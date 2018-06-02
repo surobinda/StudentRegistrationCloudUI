b37f0c82aad2fabbe459c40589c50776781f57a7
surobinda <surobinda@gmail.com>
2018-06-02 21:12:01 +0530

    Initial version

---
+++ component/StudentRegistrationApp.js
@@ -0,0 +1,9 @@
+import React from from 'react';
+
+class StudentRegistrationApp extends React.Component {
+    render() {
+        return null;
+    }
+}
+
+export default StudentRegistrationApp;
---
+++ index.js
@@ -0,0 +1,8 @@
+import StudentRegistrationApp from './component/StudentRegistrationApp.js';
+
+const exported = {
+    StudentRegistrationApp
+};
+
+export default exported;
+export {StudentRegistrationApp};
